d8537638abce8ff1247251f38082f9977a74e80c
matsuaya84 <83041975+matsuaya84@users.noreply.github.com>
2025-01-07 09:40:57 +0900

    Update fgetcsv.php

--- fgetcsv.php
+++ fgetcsv.php
@@ -5,8 +5,8 @@ if (($handle = fopen("test.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle)) !== FALSE) {
         $num = count($data);
         $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
+        for ($i=0; $i < $num; $i++) {
+            echo $data[$i] . "<br />\n";
         }
     }
     fclose($handle);
